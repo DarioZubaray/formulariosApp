1dfc46a7e8d9f8b61457de4fded73cc84367ca3e
Dario Jose Zubaray <dzubaray@gmail.com>
2018-10-31 09:06:05 -0300

    creacion del formulario - formGroup

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma : FormGroup;
+
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('Dario'),
+      'appelido': new FormControl(),
+      'correo': new FormControl()
+    });
+  }
 
   ngOnInit() {
   }
 
+  guardarCambios(){
+    console.log( this.forma.value );
+  }
 }
