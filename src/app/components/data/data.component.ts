2ccb9ddf19371833e3cf9cd121a63ab6539e4122
Dario Jose Zubaray <dzubaray@gmail.com>
2018-11-01 08:44:37 -0300

    agrupacion de objetos con formgroupname

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,14 +9,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 export class DataComponent implements OnInit {
 
   forma : FormGroup;
+  usuario: Object = {
+    'nombreCompleto': {
+      'nombre': 'Dario',
+      'apellido': 'Zubaray'
+    },
+    'correo': 'dzubaray@gmail.com'
+  }
 
   constructor() {
+    console.log("Usuario: " + this.usuario);
+
     this.forma = new FormGroup({
-      'nombre': new FormControl( 'Dario', [
-        Validators.required,
-        Validators.minLength(3)]
-      ),
-      'apellido': new FormControl( '', Validators.required ),
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl( 'Dario', [
+          Validators.required,
+          Validators.minLength(3)]
+        ),
+        'apellido': new FormControl( '', Validators.required )
+      }),
       'correo': new FormControl( '', [
         Validators.required,
         Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
